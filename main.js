d8b391255d8e4f80509838ad9f460ef98a13b9ff
Asif <khanasif1654@gmail.com>
2023-08-22 12:02:12 +0530

    This is joke generator

---
+++ main.js
@@ -0,0 +1,39 @@
+let h1 = document.querySelector('h1')
+let ul = document.querySelector('ul')
+let li = document.querySelector('li')
+let button = document.querySelector('button')
+h1.style.color='white'
+h1.style.textTransform="uppercase"
+
+fetch(
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+)
+  .then(function (response) {
+    console.log(response); // we are getting the response on the console but it is the half data for getting the full data we need to add .json to wait for all the data as the data is in the packets that is why we are getiting the data but in meta data form or in the form of header
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    console.log(data.joke)
+
+    function domMan(){
+
+      h1.append(data.joke)
+
+       button.addEventListener('click',function(){
+
+        location.reload();
+
+       })
+
+    }
+    domMan()
+
+    //category
+    //joke
+
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
